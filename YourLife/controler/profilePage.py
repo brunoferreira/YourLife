07ea31a919735bb5e0bc55d4a20a835bd109212c
bruno <bgaccount@hotmail.com>
2022-07-08 04:44:11 -0300

    Users can make posts now

--- YourLife/controler/profilePage.py
+++ YourLife/controler/profilePage.py
@@ -1,13 +1,16 @@
-from flask import render_template, session
+from flask import render_template, session, request
 from YourLife import app
 from YourLife.model import queries
 
-@app.route("/profile/<username>")
+@app.route("/profile/<username>", methods=['GET', 'POST'])
 def profile(username):
     profile_photo = queries.get_profile_photo(username)
     summary = queries.get_summary(username)
     album = queries.get_album_photos(username)
-    posts = queries.get_posts(username)
     full_name = queries.get_full_name(username)
 
+    if request.method == 'POST' and request.form['content']:
+        queries.add_post(username, request.form['content'])
+
+    posts = queries.get_posts(username)
     return render_template('profilePage.html', profile_photo=profile_photo, summary=summary, album=album, posts=posts, full_name=full_name)
